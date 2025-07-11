e72c69d3cde6d88ddc3ca384f37ac451eabd381a
Edward <youuung.fella@gmail.com>
2025-07-11 12:41:43 +0500

    fixed mason-lspconfig api

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,31 +1,50 @@
+require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "gopls", "pyright", "bashls", "lua_ls"},
-	automatic_installation = true,
+	ensure_installed = {},
 })
 
--- Настройка LSP через mason-lspconfig
-require('mason-lspconfig').setup_handlers({
-	-- Базовые настройки для всех LSP
-	function (server_name)
-		require('lspconfig')[server_name].setup({})
-	end,
-	-- Специальные настройки только для gopls
-	gopls = function()
-		require('lspconfig').gopls.setup({
-			settings = {
-				gopls = {
-					analyses = {
-						loopvar = true,  -- Показывает предупреждения для "неидиоматических" циклов
-					},
-					hints = {
-						assignVariableTypes = true,
-						compositeLiteralFields = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						rangeVariableTypes = true,  -- Подсказывает использовать `range` вместо C-style for
-					},
+-- Настройка LSP серверов
+local lspconfig = require('lspconfig')
+
+-- Базовые настройки для всех серверов
+-- local default_setup = function(server)
+	-- lspconfig[server].setup({})
+-- end
+
+-- Специальные настройки для конкретных серверов
+local servers = {
+	gopls = {
+		settings = {
+			gopls = {
+				analyses = {
+					loopvar = true,
+				},
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					rangeVariableTypes = true,
 				},
 			},
-		})
-	end,
-})
+		},
+	},
+	-- Можно добавить другие серверы с особыми настройками здесь
+}
+
+-- Применяем настройки
+vim.lsp.config("gopls", {settings = {
+	gopls = {
+		analyses = {
+			loopvar = true,
+		},
+		hints = {
+			assignVariableTypes = true,
+			compositeLiteralFields = true,
+			constantValues = true,
+			functionTypeParameters = true,
+			rangeVariableTypes = true,
+		},
+	},
+},
+	} )
