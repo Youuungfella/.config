35ff2c704d1692d5c600e0136aae0d86330b388b
Edward <youuung.fella@gmail.com>
2026-06-15 00:19:55 +0500

    treesitter added autocommands for starting highligtning

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
-require('nvim-treesitter').setup{
+require('nvim-treesitter').setup {
 	highlight = { enable = true },
 	auto_install = true,
 	ensure_installed = { "lua", "go", "python", "vimdoc", "vim", "markdown" }
 }
-
-
+-- Autocommand to start highlightning
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { "lua", "go", "python", "vimdoc", "vim", "markdown" },
+	callback = function() vim.treesitter.start() end,
+})
