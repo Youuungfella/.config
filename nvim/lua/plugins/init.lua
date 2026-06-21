6067d8d819e6253f0568b666ef1fc73e033c111f
Edward <youuung.fella@gmail.com>
2026-06-21 13:38:30 +0500

    hcon added yazi plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -136,6 +136,35 @@ require("lazy").setup({
 		end
 	},
 
+	--Yazy explorer
+
+	{
+		"mikavilpas/yazi.nvim",
+		version = "*", -- use the latest stable version
+		event = "VeryLazy",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", lazy = true },
+		},
+		keys = {
+			{
+				"<leader>-",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				"<c-up>",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		},
+	},
+
 	-- Plugin for GO
 	{
 		'ray-x/go.nvim',
