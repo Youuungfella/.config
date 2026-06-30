a08566b7e10a71832de98264ee6afdda87b99f9e
Edward <youuung.fella@gmail.com>
2026-07-01 00:04:51 +0500

    colorscheme custom Type hl and borders for mason, transperent bg for telescope

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,22 @@
 require("catppuccin").setup({
 	flavour = "macchiato", -- latte, frappe, macchiato, mocha
-	background = {         -- :h background
+	background = {      -- :h background
 		light = "macchiato",
 		dark = "macchiato",
 	},
 	transparent_background = true, -- disables setting the background color.
-	show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-	term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = true,         -- sets terminal colors (e.g. `g:terminal_color_0`)
 	dim_inactive = {
-		enabled = false,           -- dims the background color of inactive window
+		enabled = false,        -- dims the background color of inactive window
 		shade = "dark",
-		percentage = 0.15,         -- percentage of the shade to apply to the inactive window
+		percentage = 0.15,      -- percentage of the shade to apply to the inactive window
 	},
-	no_italic = false,             -- Force no italic
-	no_bold = false,               -- Force no bold
-	no_underline = false,          -- Force no underline
-	styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-		comments = { "italic" },   -- Change the style of comments
+	no_italic = false,          -- Force no italic
+	no_bold = false,            -- Force no bold
+	no_underline = false,       -- Force no underline
+	styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
 		conditionals = { "italic" },
 		loops = {},
 		functions = {},
@@ -31,17 +31,36 @@ require("catppuccin").setup({
 		-- miscs = {}, -- Uncomment to turn off hard-coded styles
 	},
 	color_overrides = {},
-	custom_highlights = {},
+	custom_highlights = function(colors)
+		return {
+			-- Custom color for Type
+			["@type"] = { fg = colors.red },
+			["@type.builtin"] = { fg = colors.red },
+			Type = { fg = colors.red },
+
+			-- Mason boarder and transparent background 
+			MasonNormal = { bg = "NONE" },
+			MasonBorder = { fg = colors.blue, bg = "NONE" },
+			TelescopeNormal = { bg = "NONE"},
+		}
+	end,
 	default_integrations = true,
 	integrations = {
 		cmp = true,
 		gitsigns = true,
 		nvimtree = true,
 		treesitter = true,
-		notify = false,
+		notify = true,
+		dap = true,
+		dap_ui = true,
+		mason = true,
+		fzf = true,
 		mini = {
 			enabled = true,
 			indentscope_color = "",
+		},
+		telescope = {
+			enabled = true
 		},
 		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 	},
