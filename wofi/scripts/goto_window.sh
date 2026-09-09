74b627d98c3d97d97cff5ff14360fd9190c17947
Edward <youuung.fella@gmail.com>
2026-09-09 22:53:35 +0500

    wofi script fixed

--- wofi/scripts/goto_window.sh
+++ wofi/scripts/goto_window.sh
@@ -1,5 +1,8 @@
-hyprctl clients -j | jq -r '.[] | "\(.workspace.name) | \(.class) | \(if (.title | length) > 50 then .title[:50] + "..." else .title end)" +
-" | \(.address)"' | \
+#!/bin/bash
+# ~/.config/hypr/scripts/goto_window.sh
+
+hyprctl clients -j | jq -r '.[] | "\(.workspace.name) | \(.class) | \(if (.title | length) > 50 then .title[:50] + "..." else .title end)" + " | \(.address)"' | \
 wofi -i --dmenu --prompt="Окна:" --width=1400 --hide-scroll --no-actions | \
 awk -F'|' '{gsub(/^[ \t]+|[ \t]+$/, "", $4); print $4}' | \
-xargs -I {} hyprctl dispatch focuswindow address:{}
+xargs -I {} hyprctl dispatch "hl.dsp.focus({ window = \"address:{}\" })"
+
